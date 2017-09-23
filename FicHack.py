ed5c489b96dc8b76642fb279dea4b36cd09f4210
Isabelle Barrett <isabelle.barrett@kcl.ac.uk>
2017-09-24 00:25:50 +0100

    second commit, added script to print story text

--- FicHack.py
+++ FicHack.py
@@ -1,4 +1,6 @@
 import requests
+import re
+
 url = 'https://github.com/kennethreitz/requests/'
 payload = {'key1': 'value1', 'key2': 'value2'}
 r = requests.get("https://www.fanfiction.net/s/10004326/1/The-Very-Best")
@@ -7,5 +9,11 @@ r = requests.get("https://www.fanfiction.net/s/10004326/1/The-Very-Best")
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(r.content, "html.parser")
 result= soup.find("div", {"id": "storytext"})
-print(result.prettify())
 #parse contents with beautifulsoup and print only the text under "storytext" id
+
+story = result.prettify
+regex = re.compile( '\s*<[^>]+>\s*')
+cleanedStory=regex.sub("\n",str(story))
+#cleanes output of result.prettify to remove tags
+
+print cleanedStory
