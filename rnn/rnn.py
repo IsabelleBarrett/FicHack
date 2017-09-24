1794fd23e9f2269b52af8e07f015ca72cfe53f2d
ckendrick <callumkendrick@hotmail.co.uk>
2017-09-24 02:47:45 +0100

    help

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -13,7 +13,30 @@ def build_dataset(words):
 
     return dictionary, reverse_dictionary
 
+def RNN(x, weights, biases):
+    x = tf.reshape(x, [-1, n_inputs])
+    x = tf.split(x, n_inputs, 1)
+
+    rnn_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden)
+
+    outputs, states = tf.contrib.rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
+
+    return tf.matmul(outputs[-1], weights["out"]) + biases["out"]
+
 import re
 words = re.findall(r'\w+', open('sample.txt').read().lower())
 
-print(build_dataset(words)[0])
+dictionary, reverse_dictionary = build_dataset(words)
+
+vocab_size = len(dictionary)
+n_inputs = 3
+n_hidden = 512
+
+weights = {
+    "out": tf.Variable(tf.random_normal([n_hidden, vocab_size]))
+}
+biases = {
+    "out": tf.Variable(tf.random_normal([vocab_size]))
+}
+
+print(RNN(dictionary, weights, biases))
